d170424172926318e89f9c18c28fe07106662ca0
Dawid Szczęśniak <Dawid Szczniak>
2016-06-29 15:35:58 +0200

    init

---
+++ desktop/src/pl/dawidszczesniak/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.dawidszczesniak.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.dawidszczesniak.TutorialClickerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialClickerGame(), config);
+	}
+}
