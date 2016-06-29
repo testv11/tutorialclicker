25268615344b20d3b72e0026fb30a55f25e56016
Dawid Szczęśniak <Dawid Szczniak>
2016-06-29 20:30:49 +0200

    config desktop launcher

--- core/src/pl/dawidszczesniak/TutorialClickerGame.java
+++ core/src/pl/dawidszczesniak/TutorialClickerGame.java
@@ -1,33 +1,35 @@
 package pl.dawidszczesniak;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class TutorialClickerGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import pl.dawidszczesniak.screens.SplashScreen;
+
+public class TutorialClickerGame extends Game {
+	
+	public final static String GAME_NAME = "Tutorial CLicker";
+	
+	public final static int WIDTH = 480;
+	public final static int HEIGHT = 700;
+	
+	private boolean paused;
+	
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+this.setScreen(new SplashScreen(this));
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+
+
+
+/* getters and setters */
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
 	}
 }
--- desktop/src/pl/dawidszczesniak/desktop/DesktopLauncher.java
+++ desktop/src/pl/dawidszczesniak/desktop/DesktopLauncher.java
@@ -7,6 +7,13 @@ import pl.dawidszczesniak.TutorialClickerGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+config.title = TutorialClickerGame.GAME_NAME;
+config.width = TutorialClickerGame.WIDTH;
+config.height = TutorialClickerGame.HEIGHT;
+config.resizable = false;
+
+		
 		new LwjglApplication(new TutorialClickerGame(), config);
 	}
 }
